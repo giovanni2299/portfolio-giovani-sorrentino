174eb3bdfbc7a94f320f11a85fc0b7c486012e7c
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-09-30 11:13:20 +0200

    ADD: modify

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -14,12 +14,14 @@
         <div class="ms_col row col-3 text-center">
             
             <div class="col-6">
-                <a href="/img/Giovanni_Sorrentino CV.pdf">
+                <a class="ms_a" href="/img/Giovanni_Sorrentino_CV.pdf" download="Giovanni_Sorrentino_CV.pdf">
                     IT_CV
-
                 </a>
             </div>
-            <div class="col-6">EN_CV</div>
+            <div class="col-6">
+                <a href=""></a>
+                EN_CV
+            </div>
         </div>
   
     
@@ -35,6 +37,11 @@
 .ms_col{
     align-self: center;
     font-size: 20px;
+}
+
+.ms_a{
+    text-decoration: none;
+    color: currentColor;
 }
 
 .logo{
